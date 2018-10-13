49e5392b31d7b95c22b206dbc2675e227a4e5989
geundragon <geundragon@naver.com>
2018-10-13 21:20:49 +0900

    added find_mmethod_tests' structures in DatabaseTest.java

--- src/test/java/ac/knu/service/DatabaseTest.java
+++ src/test/java/ac/knu/service/DatabaseTest.java
@@ -1,7 +1,25 @@
 package ac.knu.service;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class DatabaseTest {
 
+    private Database database;
+
+    @Before
+    public void setUp(){
+        database = new Database();
+    }
+
+    // find method tests..
+    @Test
+    public void 봇은_find_친구이름_명령어를_요청받았을때_친구이름이_리스트에있으면_친구이름을_반환해야한다(){
+
+    }
+
+    @Test
+    public void 봇은_find_친구이름_명령어를_요청받았을때_친구이름이_리스트에없으면_NULL을_반환해야한다(){
+
+    }
 }
